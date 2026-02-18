0dd5cafad4272bf023c5ffb66fa6b5757549d02f
Ricky <rikkii.takamoto7589@gmail.com>
2026-02-18 13:30:36 +0900

    var構造体にdatatypeを保存できるようにすると共にそれに付随した定数定義を行う

--- inc/nstt_parser_config-component.h
+++ inc/nstt_parser_config-component.h
@@ -4,6 +4,15 @@
 
     #include"nstt_parser.h"
 
+    //データ型識別用のdefine
+    #define NSTT_TYPE_NONE   0
+    #define NSTT_TYPE_INT    1
+    #define NSTT_TYPE_CHAR   2
+    #define NSTT_TYPE_DOUBLE 3
+    #define NSTT_TYPE_STRING 4
+
+
+
     typedef struct _Date{
         short int year;
         short int month;
@@ -20,6 +29,15 @@
     //時間を時間・分・秒単位までで保存することのできるデータ型
 
     typedef struct _Variable{
+        unsigned char data_type;
+        /*
+            * 0: NONE   未定義
+            * 1: INT    整数型
+            * 2: CHAR   bytecode
+            * 3: DOUBLE 浮動小数点
+            * 4: STRING 文字列: max 8192 bytes
+        */
+
         int int_data;
         char char_data;
         double double_data;
