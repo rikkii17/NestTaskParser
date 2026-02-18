0eef51a00af88c5c66c6ecb2be26c2deb0687ae7
Ricky <rikkii.takamoto7589@gmail.com>
2026-02-18 13:04:03 +0900

    config敬の修正 	modified:   inc/nstt_parser_config.h 		主に構造体の修正。コメントの追記

--- inc/nstt_parser_config.h
+++ inc/nstt_parser_config.h
@@ -1,44 +1,43 @@
 #ifndef NSTT_PARSER_CONFIG_H
-    #define NSTT_PARSER_CONFIG_H
+#define NSTT_PARSER_CONFIG_H
 
-    #include"nstt_parser_config-component.h"
+#include "nstt_parser_config-component.h"
 
-    typedef struct _Nest_task_data_struct{
-        //コンテンツ部
-        char task_name[1024];
-        Deadline_time deadline_time;
-        char importance_level;
-        Time need_time;
-        Time worked_time;
-        char memo[4096];
-        Option_data option_data;
+// 前方定義
+typedef struct _Option_data Option_data;
+typedef struct _Nest_task_data Nest_task_data;
 
-        //リスト部
-        Nest_task_data *above_data;
-        Nest_task_data *under_data;
-        Nest_task_data *next_data;
-        Nest_task_data *previous_data;
+// 期限管理用構造体
+typedef struct _Deadline_time {
+    Date deadline_date;
+    short int hour;
+    short int minute;
+} Deadline_time;
 
-    } Nest_task_data;
-    //ネストタスクデータを一括で保存することのできる構造体
+// オプションデータ
+struct _Option_data {
+    char option_word[256]; // <option_word> を保持
+    var option_content;
+    Option_data *next_data;
+    Option_data *previous_data;
+};
 
-    typedef struct _Deadline_time{
-        Date deadline_date;
-        short int hour;
-        short int minute;
-    } Deadline_time;
-    //デッドラインを保存するための構造体
+// メインタスク構造体
+struct _Nest_task_data {
+    // コンテンツ
+    char task_name[1024];
+    Deadline_time deadline_time;
+    unsigned char importance_level; 
+    Time need_time;
+    Time worked_time;
+    char memo[4096];
+    Option_data *option_head; // オプションリストの先頭ポインタに変更
 
-    typedef struct _Option_data{
-        var option_content;
-        Option_data *next_data;
-        Option_data *previous_data;
-    } Option_data;
-
-    
-
-
-
-    
+    // リスト・ツリー構造
+    Nest_task_data *parent;    // 上の階層へのポインタ
+    Nest_task_data *children;  // 下の階層へのポインタ
+    Nest_task_data *next;      // 同階層の次のタスク
+    Nest_task_data *prev;      // 同階層の前のタスク
+};
 
 #endif
